66c390a424da92a53d8db37bfab3617fd634e1b4
Nikita job <cycyrbita@mail.ru>
2021-06-21 13:04:53 +0300

    чистка js + картинки

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,5 @@
 
 $(document).ready(function () {
-    $('.nav__burger').click(function() {
-        $('.nav__list_header').toggleClass('active');
-        $('.nav__burger').toggleClass('active');
-    })
-
     $('.js-message__box').owlCarousel({
         loop: true,
         autoHeight: true,
@@ -29,39 +24,4 @@ $(document).ready(function () {
             }
         }
     });
-
-    $('.form__btn').click(function(e) {
-        e.preventDefault();
-        $('.form').find('.form__field').each(function () {
-            if ($(this).val() != '') {
-                $(this).removeClass('form__field_empty');
-            } else {
-                $(this).addClass('form__field_empty');
-            }
-        });
-
-        if(!$('.form__field').hasClass('form__field_empty')) {
-            $('.form__field').val('');
-        }
-    })
-
-    $('.footer__modal-item').click(function() {
-        var $index = $(this).index();
-        $('.footer-modal').removeClass('active');
-        $('.footer-modal').eq($index).addClass('active');
-        $('body, html').addClass('overflow');
-    })
-
-    $('.footer-modal__close').click(function() {
-        $('.footer-modal').removeClass('active');
-        $('body, html').removeClass('overflow');
-    })
-
-    $(document).mouseup(function(e) {
-		var div = $('.footer-modal__body');
-		if (!div.is(e.target) && div.has(e.target).length === 0) {
-			$('.footer-modal').removeClass('active');
-            $('body, html').removeClass('overflow');
-		}
-	});
 });
